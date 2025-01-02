f2251732f3eaff37a37d8ddb8c2b808ec5366560
RYUJEONGHUN <ryugh0210@naver.com>
2025-01-03 03:39:45 +0900

    첫 커밋

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Book
+from .models import FineTunedModel, TrainingData
 
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  list_display=['title','author','publication_date','price']
+  search_fields=['title','author']
+  list_filter=['author','publication_date']
 # Register your models here.
+
+@admin.register(FineTunedModel)
+class FineTunedModelAdmin(admin.ModelAdmin):
+  list_display=('model_name','base_model')
+  search_fields=('model_name','base_model')
+
+
+@admin.register(TrainingData)
+class TrainingDataAdmin(admin.ModelAdmin):
+  list_display=('prompt','completion','fine_tuned_model')
+  search_fields=('prompt','completion','fine_tuned_model__model_name')
+  list_filter=('fine_tuned_model',)
--- myapp/models.py
+++ myapp/models.py
@@ -6,4 +6,28 @@ class Book(models.Model):
   author=models.CharField(max_length=200)
   publication_date=models.DateField()
   price=models.DecimalField(max_digits=6,decimal_places=2)
+
+
+class FineTunedModel(models.Model):
+  Model_CHOICES=[
+    ('ada','Ada'),
+    ('babbage','Babbage'),
+    ('curie','Curie'),
+    ('davinci','Davinci'),
+  ]
+
+  model_name=models.CharField(max_length=100)
+  base_model=models.CharField(max_length=100, choices=Model_CHOICES)
+
+  def __str__(self):
+    return self.model_name
+  
+class TrainingData(models.Model):
+  fine_tuned_model=models.ForeignKey(FineTunedModel, on_delete=models.CASCADE, related_name='training_data')
+  prompt=models.TextField()
+  completion=models.TextField()
+
+  def __str__(self):
+    return f"{self.fine_tuned_model.model_name}의 훈련 데이터"
 # Create your models here.
+
